cc2599a9bcd6f372635223b190502dea8a59e69e
andystrn <an.strnadlova@gmail.com>
2023-02-17 12:24:49 +0100

    content divided to components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import './style.css';
+import './style.css'
+import { Banner } from './components/Banner';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
+import { Gallery } from './components/Gallery';
+import { Header } from './components/Header';
+import { Menu } from './components/Menu';
 
 console.log('funguju!');
+
+const pageElement = document.createElement('div');
+pageElement.classList.add('page');
+
+const main = document.createElement('main');
+main.append(Banner(), Menu(), Gallery(), Contact());
+
+pageElement.append(Header(), main, Footer());
+
+document.querySelector('#app').append(pageElement);
