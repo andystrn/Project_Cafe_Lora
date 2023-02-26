ef214c6900ffa4ca29e1830a9d0fb85847618f47
andystrn <an.strnadlova@gmail.com>
2023-02-26 15:41:31 +0100

    adding functionality of order button

--- src/components/Menu/components/Drink/index.js
+++ src/components/Menu/components/Drink/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 import { Layer } from '../Layer';
 
-export const Drink = ( {name, image, layers, ordered} ) => {
+export const Drink = ( {id, name, image, layers, ordered} ) => {
     const element = document.createElement('div');
     element.classList.add('drink');
     element.innerHTML = `
@@ -14,13 +14,27 @@ export const Drink = ( {name, image, layers, ordered} ) => {
                     <div class="layers"><div/>
                 </div>
             </div>
-            <div class="drink__controls">
-                    <button class="order-btn">
-                    Objednat
+            <div class= "drink__controls">
+                    <button class= "order-btn ${ordered ? 'order-btn--ordered' : null}">
+                    ${ordered ? 'Zrušit' : 'Objednat'}
                     </button>
             </div>
     `
     element.querySelector('.layers').append(...layers.map((item) => Layer(item)));
+
+    element.querySelector('.order-btn').addEventListener('click', () => {
+        fetch(`https://cafelora.kodim.app/api/me/drinks/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Email andrea.strnadlova@gnj.cz',
+            },
+            body: JSON.stringify({ordered: !ordered})
+        })
+        .then((response) => response.json())
+        .then((data) => element.replaceWith(Drink(data.results)))
+    }  
+    );
 
     return element;
 }
